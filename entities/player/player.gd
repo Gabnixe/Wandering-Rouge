aeafcbb997d8d2a86be6152f3946b4434fdfd9e3
Gabnixe <gabnixe@gmail.com>
2023-12-04 20:47:32 -0500

    Rework of UI structure

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -8,23 +8,28 @@ var gameLoopManager : GameLoopManager
 func _ready():
 	inputManager = get_node("/root/InputManager")
 	gameLoopManager = get_node("/root/GameLoopManager")
+	gameLoopManager.player = self
 	inputManager.input_received.connect(_on_input_received)
 
 func _on_input_received(input : InputManager.InputType):
 	inputManager.isWaitingForInput = false;
 	if(input == InputManager.InputType.ACTION_UP_LEFT):
-		tweenAnims.playMoveTween(self,position, Vector2.UP + Vector2.LEFT, gameLoopManager.endStep)
+		move_player(Vector2.UP + Vector2.LEFT)
 	if(input == InputManager.InputType.ACTION_UP):
-		tweenAnims.playMoveTween(self,position, Vector2.UP, gameLoopManager.endStep)
+		move_player(Vector2.UP)
 	if(input == InputManager.InputType.ACTION_UP_RIGHT):
-		tweenAnims.playMoveTween(self,position, Vector2.UP + Vector2.RIGHT, gameLoopManager.endStep)
+		move_player(Vector2.UP + Vector2.RIGHT)
 	if(input == InputManager.InputType.ACTION_LEFT):
-		tweenAnims.playMoveTween(self,position, Vector2.LEFT, gameLoopManager.endStep)
+		move_player(Vector2.LEFT)
 	if(input == InputManager.InputType.ACTION_RIGHT):
-		tweenAnims.playMoveTween(self,position, Vector2.RIGHT, gameLoopManager.endStep)
+		move_player(Vector2.RIGHT)
 	if(input == InputManager.InputType.ACTION_DOWN_LEFT):
-		tweenAnims.playMoveTween(self,position, Vector2.DOWN + Vector2.LEFT, gameLoopManager.endStep)
+		move_player(Vector2.DOWN + Vector2.LEFT)
 	if(input == InputManager.InputType.ACTION_DOWN):
-		tweenAnims.playMoveTween(self,position, Vector2.DOWN, gameLoopManager.endStep)
+		move_player(Vector2.DOWN)
 	if(input == InputManager.InputType.ACTION_DOWN_RIGHT):
-		tweenAnims.playMoveTween(self,position, Vector2.DOWN + Vector2.RIGHT, gameLoopManager.endStep)
+		move_player(Vector2.DOWN + Vector2.RIGHT)
+
+func move_player(direction : Vector2):
+	tweenAnims.playMoveTween(self,position, direction, gameLoopManager.endStep)
+	gameLoopManager.camera.moveCamera(direction)
