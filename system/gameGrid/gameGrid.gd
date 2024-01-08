7b32985bb94dcaa33196878871b0b95c15f0763d
Gabnixe <gabrielstlaurentrecoura@gmail.com>
2024-01-08 16:38:48 -0500

    Basic room creation implmentation

--- system/gameGrid/gameGrid.gd
+++ system/gameGrid/gameGrid.gd
@@ -3,12 +3,57 @@ extends Node
 
 const array2D = preload("res://utils/array2D.gd")
 
-var grid : Array
-var gridSize : Vector2 = Vector2(10,10)
+var gameGrid : Array
+var gridSize : Vector2i = Vector2i(20,20)
 
 func _ready():
-	grid = array2D.create_array2D(gridSize.x,gridSize.y, 1)
-	grid[4][4] = 0
-	array2D.print_array2D(grid)
-	
+	for n in 10:
+		gameGrid = array2D.create_array2D(gridSize.x,gridSize.y, "■")
+		var gridRectangle = Rect2i(Vector2i.ZERO, Vector2i(gridSize.x, gridSize.y))
+		divideGridRecursive(gameGrid,gridRectangle)
+		array2D.print_array2D(gameGrid)
+		print(" ")
+		print(" ")
+		print(" ")
+		
+func divideGridRecursive(gameGrid:Array, grid:Rect2i):
+	if(grid.size.x >= 10 && grid.size.y >= 10):
+		var subGrids = cutGrid(grid)
+		subGrids[0] = divideGridRecursive(gameGrid,subGrids[0])
+		subGrids[1] = divideGridRecursive(gameGrid,subGrids[1])
+		return subGrids
+	else:
+		createRoom(gameGrid, grid)
+		return grid
 
+func cutGrid(grid:Rect2i): 
+	var grid1
+	var grid2
+	var gridIsWiderThanTall = false
+	if grid.size.x == grid.size.y:
+		gridIsWiderThanTall = randi() % 2 == 0
+	else:
+		gridIsWiderThanTall = grid.size.x > grid.size.y
+		
+	if gridIsWiderThanTall:
+		var cutPoint = randi_range(5, grid.size.x - 5)
+		grid1 = grid.grow_side(SIDE_LEFT, -cutPoint)
+		grid2 = grid.grow_side(SIDE_RIGHT, -(grid.size.x - cutPoint))
+	else:
+		var cutPoint = randi_range(5, grid.size.y - 5)
+		grid1 = grid.grow_side(SIDE_BOTTOM, -cutPoint)
+		grid2 = grid.grow_side(SIDE_TOP, -(grid.size.y - cutPoint))
+	return [grid1, grid2]
+
+func createRoom( grid:Array, zone:Rect2i ):
+	var XRoomStart = randi_range(1, zone.size.x / 2 - 1)
+	var XRoomEnd = randi_range(zone.size.x / 2 + 1, zone.size.x - 2)
+	var YRoomStart = randi_range(1, zone.size.y / 2 - 1)
+	var YRoomEnd = randi_range(zone.size.y / 2 + 1, zone.size.y - 2)
+	XRoomStart += zone.position.x
+	XRoomEnd += zone.position.x
+	YRoomStart += zone.position.y
+	YRoomEnd += zone.position.y
+	for x in range(XRoomStart,XRoomEnd + 1):
+		for y in range(YRoomStart,YRoomEnd + 1):
+			grid[x][y] = " "
--- utils/array2D.gd
+++ utils/array2D.gd
@@ -14,9 +14,9 @@ static func create_array2D(width : int, height : int, fillObject = null):
 	return array2D
 	
 static func print_array2D(array2D):
-	for x in array2D.size():
+	for y in array2D[0].size():
 		var output = ""
-		for y in array2D[x].size():
+		for x in array2D.size():
 			output += str(array2D[x][y])
 			output += "	"
 		print(output)
